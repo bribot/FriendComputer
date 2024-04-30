d8eb49e4f35267f717e6fe96d727e9caf4e563ee
bribot <bri.medrano@gmail.com>
2024-04-30 13:58:19 -0600

    better dice bag

--- diceBag.py
+++ diceBag.py
@@ -8,7 +8,7 @@ import random as ran
 
 def test():
     # msj = str(ran.randint(1,10))+'+'+str(ran.randint(1,5))+'d'+str(ran.randint(1,20))
-    msj= '5*1d4'
+    msj= '1d20-1'
     print("Running...")
     print(msj)
     print(reconDice(msj))
@@ -16,51 +16,58 @@ def test():
 
 def rollDice(msj):
     msj = reconDice(msj)
-    msj2=[0,'']
-    for m in msj.split('+'):
-        if m.isnumeric():
-            msj2[0]=msj2[0]+int(m)
-            msj2[1]=msj2[1]+m+'+'
-        else:
-            tmp = eval(m)
-            msj2[0] = msj2[0]+tmp[0]
-            msj2[1] = msj2[1]+str(tmp[1])+'+'
-    msj2[1]=msj2[1][:-1]
-    return msj2
+    msj2 = eval(msj)
+    return [msj2,msj]
+
 
 def reconDice(msj):
-    index=0
-    m=msj.split('d')
-    while((len(msj.split('d'))-index-1)!=0):
-        tmp=-1
-        while(True):
-            if (tmp+len(m[index])+1)==0 or not m[index][tmp].isnumeric():
-                m1 = m[index][tmp+1:]
-                break
-            tmp-=1
-        m[index]=m[index][0:tmp+1]+'getRoll('+m1+','
-        tmp=0
-        while(True):
-            if (tmp-len(m[index+1]))==0 or not m[index+1][tmp].isnumeric():
-                m2 = m[index+1][0:tmp]
-                break
-            tmp+=1
-        m[index+1]=m2+')'+m[index+1][tmp:]
-        m[index+1]=m[index]+m[index+1]
-        msj2=m[index+1]
-        m[index]=''
-        index+=1
-    msj = msj2
-    return msj                
+    mes = msj.split('d')
+    nRoll = len(mes)-1
+    result = ''
+    facedice = 0
+    dice = -1
+    face = -1
+    for m in mes:
+        m1=''
+        m2=''
+        if(facedice == 1):
+            while(facedice == 1):
+                if(len(m)>0 and m[0].isnumeric()):
+                    temp,m = m[0],m[1:]
+                    m2 = m2+temp
+                else:
+                    face = int(m2)
+                    mR = getRoll(dice,face)
+                    dice = -1
+                    face = -1
+                    result = result + mR
+                    nRoll-=1
+                    facedice = 0
+            facedice = 0 if nRoll>0 else 1
+        if(facedice == 0):
+            while(facedice == 0):
+                if(len(m)>0 and m[-1].isnumeric()):
+                    temp,m = m[-1],m[:-1]
+                    m1 = temp+m1
+                else:
+                    dice = int(m1)
+                    facedice = 1
+        result = result + m
+        
+    return result
+
+                    
 
 def getRoll(ndice, nfaces):
     dice = []
-    result = 0
+    result = '('
     for n in range(ndice):
         die = ran.randint(1,nfaces)
         dice.append(die)
-        result += die
-    return result,dice
+        result += str(die) + "+"
+    result = result[0:-1]
+    result += ")"
+    return result#,dice
 
 if __name__ == '__main__':
-    main()
+    test()
